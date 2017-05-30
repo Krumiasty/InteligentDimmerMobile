e9596bcdc0615d85d70eaacf127d606f3017ef6f
Krumiasty <krruuum@wp.pl>
2017-05-30 16:23:34 +0200

    Connect to device immediately add splash screen

--- InteligentDimmerMobile/Services/PrepareDataService.cs
+++ InteligentDimmerMobile/Services/PrepareDataService.cs
@@ -4,12 +4,14 @@ namespace InteligentDimmerMobile.Services
 {
     public static class PrepareDataService
     {
-        public static void PrepareData(byte secondByte, byte fourthByte)
+        public static void PrepareData(byte secondByte, byte fourthByte, byte sixthByte)
         {
             ControlData.StartByte = 0xAA;
             ControlData.CommandByte = secondByte;
-            ControlData.SeparatorByte = 0xBB;
-            ControlData.DataByte = fourthByte;
+            ControlData.SeparatorByte1 = 0xBB;
+            ControlData.DataByte1 = fourthByte;
+            ControlData.SeparatorByte2 = 0xBB;
+            ControlData.DataByte2 = sixthByte;
             ControlData.EndByte = 0xCC;
         }
     }
